0623ccc8cb0d3606c4be057cdc056747acfad9c6
juliop11 <julioperezobrero@hotmail.com>
2023-02-13 17:11:14 +0100

    REPASO 4

--- algebraProject/point.ts
+++ algebraProject/point.ts
@@ -1,3 +1,20 @@
+// 2. Dicho fichero debe contener la clase Point con los siguientes atributos privados:
+
+// • x: number
+// • y: number
+
+// 3. El constructor de la clase debe tener los mismos parámetros de entrada que atributos.
+
+// 4. Crear los métodos setters y getters para todos los atributos de la clase.
+
+// 5. Crear un método toString() que convierta a texto las coordenadas del punto.
+// Debe devolver: “(x,y)”
+
+// 6. En otro fichero denominado pointTest.ts importar la clase Point.
+
+// 7. Crear un nuevo objeto myPoint de la clase Point, en el fichero pointTest.ts, y probar todos sus
+// métodos. Subir los cambios a GitHub.
+
 
 export class Point {
     private x: number
